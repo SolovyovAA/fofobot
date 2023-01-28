88736c3c940ae259a68bde84d1da9d2b4aca4f9d
Solovyev Aleksandr <solovyov.aa.prog@gmail.com>
2023-01-28 22:48:49 +0300

    Main: админы, фильтр, правки проверок
    Добавил фильтрацию по ID и подключил библиотеку фильтров + внес правки в проверку валидности полей пользователя

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import config
 
 import db_worker
 
+from telebot import custom_filters
+
 conf = config.Config()
 
 bot = telebot.TeleBot(conf.getToken())
 worker = db_worker.DBWorker(conf.getDatabasePath())
 worker.connectToDatabase()
+
 
 @bot.message_handler(commands=['help'])
 def help_command(message):
@@ -37,6 +40,20 @@ def get_nude(message):
     bot.send_photo(message.chat.id, 'https://www.meme-arsenal.com/memes/906057dd785388a0fa893ca85dc9b6b5.jpg');
     # bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
 
+
+# Обрабатываем команду от администратора
+@bot.message_handler(chat_id=[95597235, 1120038921],  # chat_id проверяем, в списке ли идентификатор пользователя
+                     commands=['admin'])
+def admin_rep(message):
+    bot.send_message(message.chat.id, "You are allowed to use this command.")
+
+
+# Обрабатываем команду от пользователя
+@bot.message_handler(commands=['admin'])
+def not_admin(message):
+    bot.send_message(message.chat.id, "You are not allowed to use this command")
+
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == 'привет':
@@ -46,17 +63,18 @@ def get_text_messages(message):
         username = message.from_user.username
 
         fullname = ''
-        if len( us_name ) > 0 and us_name != 'None':
-            fullname += us_name
-        if len( us_sname ) > 0 and us_sname != 'None':
-            fullname += us_sname
+        fullname += us_name if us_name is not None else ''  # Применение тернарного оператора
 
-        if worker.userExist( us_id ):
-            bot.send_message(message.from_user.id, 'Привет, %s!'%(username))
+        if us_sname is not None:
+            fullname += ' ' + us_sname
+
+        if worker.userExist(us_id):
+            bot.send_message(message.from_user.id, 'Привет, %s!' % (username))
         else:
             bot.send_message(message.from_user.id, 'Привет, %s! Ваше имя добавленно в базу данных!' % (username))
             worker.writeToDatabase(us_id, username, f'{us_name} {us_sname}', "", False)
         # bot.send_message(1120038921,'Оповещение типо =)')
+
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
@@ -65,5 +83,8 @@ def get_text_messages(message):
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 
+
+# Регистрируем фильтр
+bot.add_custom_filter(custom_filters.ChatFilter())
 
 bot.polling(none_stop=True, interval=0)
