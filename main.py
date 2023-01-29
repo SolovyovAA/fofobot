6d8bb53a2dda2fef5c400a9368a46c544e1cd835
Solovyev Aleksandr <solovyov.aa.prog@gmail.com>
2023-01-29 20:24:12 +0300

    Main: Начало реализации функционала по добавлению участников вручную

--- main.py
+++ main.py
@@ -6,11 +6,22 @@ import db_worker
 
 from telebot import custom_filters
 
+user_dict = {}
+
 conf = config.Config()
 
 bot = telebot.TeleBot(conf.getToken())
 worker = db_worker.DBWorker(conf.getDatabasePath())
 worker.connectToDatabase()
+
+
+class User:
+    def __init__(self, user_id, nickname='', name=''):
+        self.id = user_id
+        self.nickname = nickname
+        self.name = name
+        self.date = None
+        self.congratulate = None
 
 
 @bot.message_handler(commands=['help'])
@@ -67,16 +78,80 @@ def get_links(message):
 
 
 # Обрабатываем команду от администратора
-@bot.message_handler(chat_id=[95597235, 1120038921],  # chat_id проверяем, в списке ли идентификатор пользователя
+@bot.message_handler(chat_id=[95597235, 1120038921],  # chat_id проверяем, в списке ли идентификатор участника
                      commands=['admin'])
 def admin_rep(message):
-    bot.send_message(message.chat.id, "You are allowed to use this command.")
+    menu = telebot.types.InlineKeyboardMarkup()
+    menu.add(telebot.types.InlineKeyboardButton(text="Добавить данные участника", callback_data='add_user_info'))
+    menu.add(telebot.types.InlineKeyboardButton(text="Изменить данные участника", callback_data='edit_user_info'))
+    menu.add(telebot.types.InlineKeyboardButton(text="Удалить данные участника", callback_data='delete_user_info'))
+    menu.add(telebot.types.InlineKeyboardButton(text="Создать встречу", callback_data='make_event'))
+
+    bot.send_message(message.chat.id, "Что будем делать?", reply_markup=menu)
 
 
-# Обрабатываем команду от пользователя
+@bot.callback_query_handler(func=lambda call:True)
+def callback_inline(call):
+    menu = telebot.types.InlineKeyboardMarkup()
+    if call.data == "add_user_info":
+        menu.add(telebot.types.InlineKeyboardButton(text="Да", callback_data='add_with_id'))
+        menu.add(telebot.types.InlineKeyboardButton(text="Нет", callback_data='add_with_forward_message'))
+        menu.add(telebot.types.InlineKeyboardButton(text="Вернуться к упарвлению", callback_data="back_to_admin"))
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text="Занесение данных нового участника в БД.\n Известен ли ID участника?", reply_markup=menu)
+    elif call.data == "add_with_id":
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text="Введите ID участника:", reply_markup=menu)
+        bot.register_next_step_handler(call.message, process_add_id_step)
+    elif call.data == "add_with_forward_message":
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text="Перешли сообщение от участника:", reply_markup=menu)
+        bot.register_next_step_handler(call.message, process_add_message_step)
+    elif call.data == "back_to_admin":
+        admin_rep(call.message)
+
+
+def process_add_id_step(message):
+    try:
+        if message.text.isdigit():
+            chat_id = message.chat.id
+            user_id = int(message.text)
+            user = User(user_id)
+            user_dict[chat_id] = user
+            # TODO: Проверить, не существует ли такой участник уже в БД
+            msg = bot.reply_to(message, 'Введите имя участника?')
+            bot.register_next_step_handler(msg, process_add_name_step)
+        else:
+            menu = telebot.types.InlineKeyboardMarkup()
+            menu.add(telebot.types.InlineKeyboardButton(text="Вернуться в начало", callback_data="add_user_info"))
+            bot.send_message(chat_id=message.chat.id,
+                                  text="Идентификатор участника должен быть числом", reply_markup=menu)
+    except (Exception,):
+        bot.reply_to(message, "Что-то пошло не так")
+
+
+def process_add_message_step(message):
+    try:
+        # forward_date - поле
+        chat_id = message.chat.id
+        name = message.text
+        user = User(name)
+        user_dict[chat_id] = user
+        msg = bot.reply_to(message, 'How old are you?')
+        # bot.register_next_step_handler(msg, process_age_step)
+    except Exception as e:
+        bot.reply_to(message, "Что-то пошло не так")
+
+
+def process_add_name_step(message):
+    a = 1
+
+
+
+# Обрабатываем команду от участника
 @bot.message_handler(commands=['admin'])
 def not_admin(message):
-    bot.send_message(message.chat.id, "Прости, но у тебя нет прав на выполнение этой команды =)")
+    bot.send_message(message.chat.id, f"Прости {message.from_user.username}, но у тебя нет прав на выполнение этой команды =)")
 
 
 @bot.message_handler(content_types=['text'])
@@ -100,7 +175,19 @@ def get_text_messages(message):
             worker.writeToDatabase(us_id, username, fullname, "", False)
 
 
-# Регистрируем фильтр
-bot.add_custom_filter(custom_filters.ChatFilter())
+if __name__ == "__main__":
+    # Регистрируем фильтр
+    bot.add_custom_filter(custom_filters.ChatFilter())
 
-bot.polling(none_stop=True, interval=0)
+
+    # Enable saving next step handlers to file "./.handlers-saves/step.save".
+    # Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
+    # saving will happen after delay 2 seconds.
+    # bot.enable_save_next_step_handlers(delay=2)
+
+
+    # Load next_step_handlers from save file (default "./.handlers-saves/step.save")
+    # WARNING It will work only if enable_save_next_step_handlers was called!
+    # bot.load_next_step_handlers()
+
+    bot.polling(none_stop=True, interval=0)
